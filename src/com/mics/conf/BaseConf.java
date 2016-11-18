b2ac760dda783f47e59b5d803fca98e7ace66538
zjgsunjoy <1084137020@qq.com>
2016-11-18 16:12:50 +0800

    code

--- src/com/mics/conf/BaseConf.java
+++ src/com/mics/conf/BaseConf.java
@@ -3,5 +3,6 @@ package com.mics.conf;
 public class BaseConf {
 	
 	public static String baseURL = "http://192.168.1.104:8080/";
-
+	public static String username = "meinian"; 
+	public static String password = "123456";
 }
