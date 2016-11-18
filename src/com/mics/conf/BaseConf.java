66496588eb64ff7f6a5876f62bb19224d33ccfca
qingshangwushi <qingshangwushi@163.com>
2016-11-18 16:16:11 +0800

    save

--- src/com/mics/conf/BaseConf.java
+++ src/com/mics/conf/BaseConf.java
@@ -2,6 +2,6 @@ package com.mics.conf;
 
 public class BaseConf {
 	
-	public static String baseURL = "http://192.168.1.104:8080/";
+	public static String baseURL = "http://192.168.1.104/";
 
 }
--- src/com/mics/http/HttpRequest.java
+++ src/com/mics/http/HttpRequest.java
@@ -1,10 +1,23 @@
 package com.mics.http;
 
+import com.mics.conf.BaseConf;
+import com.mics.http.inter.DoctorRequest;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+
 public class HttpRequest extends BaseClass {
+	
+	private DoctorRequest doctorRequest;
+	
+	public HttpRequest() {
+		super();
+		this.doctorRequest = retrofit.create(DoctorRequest.class);
+	}
 	
 	
 	public boolean DoctorLogin(){
-		
+//		Call<ResponseBody> call = doctorRequest.doctorLogin(BaseConf., password, token)
 		return false;
 	}
 	
